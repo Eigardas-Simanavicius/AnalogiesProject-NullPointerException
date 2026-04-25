499204c56092b065d36712cb4f548e2e9d289ddf
Patrick <patrick.oconnell1@ucdconnect.ie>
2026-04-25 01:00:13 +0100

    Created testing for getNBestSourcesFor method.

---
+++ src/test/java/getNBestSourcesForTest.java
@@ -0,0 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.main.AnalogyDataHolder;
+import org.main.CompositeBuilder;
+import org.main.ConfigSetup;
+import org.main.Interfaces.Predicate;
+import org.main.MappingManager;
+import org.main.Objects.Config;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class getNBestSourcesForTest {
+    @Before
+    public void setup(){
+        ConfigSetup.setupConfig(new File("config.txt"));
+        AnalogyDataHolder.addAnalogiesFromFile(new Config());
+    }
+
+    @Test
+    public void basicTest1(){
+        long s = System.currentTimeMillis();
+        ArrayList<String> c = CompositeBuilder.getNBestSourcesFor("AIDS",10);
+        System.out.println((System.currentTimeMillis()-s)/1000.0);
+        assertEquals(10,c.size());
+        assertFalse(c.contains(null));
+    }
+
+    @Test
+    public void basicTest2(){
+        long s = System.currentTimeMillis();
+        ArrayList<String> c = CompositeBuilder.getNBestSourcesFor("Adonis",30);
+        System.out.println((System.currentTimeMillis()-s)/1000.0);
+        assertEquals(30,c.size());
+        assertFalse(c.contains(null));
+    }
+
+    @Test
+    public void largeRequestTest(){
+        long s = System.currentTimeMillis();
+        ArrayList<String> c = CompositeBuilder.getNBestSourcesFor("AIDS",100000000);
+        System.out.println((System.currentTimeMillis()-s)/1000.0);
+        assertTrue(AnalogyDataHolder.getAnalogies().size()>c.size());
+        assertFalse(c.contains(null));
+    }
+
+    @Test
+    public void fakeInputTest(){
+        long s = System.currentTimeMillis();
+        ArrayList<String> c = CompositeBuilder.getNBestSourcesFor("FakeTopicAlert",100000000);
+        System.out.println((System.currentTimeMillis()-s)/1000.0);
+        assertEquals(0,c.size());
+        assertFalse(c.contains(null));
+    }
+
+    @Test
+    public void blankInputTest(){
+        long s = System.currentTimeMillis();
+        ArrayList<String> c = CompositeBuilder.getNBestSourcesFor("",100000000);
+        System.out.println((System.currentTimeMillis()-s)/1000.0);
+        assertEquals(0,c.size());
+        assertFalse(c.contains(null));
+    }
+
+
+}
