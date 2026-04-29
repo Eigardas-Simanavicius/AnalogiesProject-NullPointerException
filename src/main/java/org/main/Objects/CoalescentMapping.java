a5051e8677ec1f398a355674b8db484e8e166472
Patrick <patrick.oconnell1@ucdconnect.ie>
2026-04-29 22:41:35 +0100

    Added useful methods ro CoalescentMapping.java

--- src/main/java/org/main/Objects/CoalescentMapping.java
+++ src/main/java/org/main/Objects/CoalescentMapping.java
@@ -1,10 +1,13 @@
 package org.main.Objects;
 
+import org.main.AnalogyManager;
 import org.main.CompositeBuilder;
 import org.main.MappingManager;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class CoalescentMapping {
 
@@ -12,7 +15,8 @@ public class CoalescentMapping {
     private String target;
     private final double richness;
     private ArrayList<String> analogies;
-    private HashMap<String,String> mapping;
+    private HashMap<String,String> coalescedMapping;
+    private HashMap<String, HashSet<String>> inferredMapping;
 
     public CoalescentMapping(String source,String target){
         this.source = source;
@@ -27,5 +31,49 @@ public class CoalescentMapping {
     public ArrayList<String> getAnalogies(){return analogies;}
     public double getRichness() {
         return richness;
+    }
+
+    public HashMap<String,String> getCoalescedMapping(){
+        if(coalescedMapping == null){
+            createCoalescedMapping();
+        }
+
+        return coalescedMapping;
+    }
+
+    private void createCoalescedMapping(){
+        coalescedMapping = new HashMap<>();
+        for(int i = 1; i < analogies.size(); i += 2){
+            coalescedMapping.put(analogies.get(i-1),analogies.get(i));
+        }
+    }
+
+    public HashMap<String,HashSet<String>> getInferredMapping(){
+        if(inferredMapping == null){
+            createInferredMapping();
+        }
+        return inferredMapping;
+    }
+
+    private void createInferredMapping(){
+        inferredMapping = new HashMap<>();
+
+        if(coalescedMapping == null) createCoalescedMapping();
+
+        coalescedMapping.forEach(this::createInferredMappingHelper);
+    }
+
+    private void createInferredMappingHelper(String sourceAnalogy, String targetAnalogy){
+        HashMap<String,String> map = MappingManager.flatStringMapping(sourceAnalogy,targetAnalogy);
+        if(map != null){
+            map.forEach(
+                    (String source, String target) -> {
+                        if(!inferredMapping.containsKey(source)){
+                            inferredMapping.put(source,new HashSet<>());
+                        }
+                        inferredMapping.get(source).add(target);
+                }
+            );
+        }
     }
 }
