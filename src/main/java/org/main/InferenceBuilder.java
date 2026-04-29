b1816bf51e215803c5b3c5da6157f783e9f08503
Patrick <patrick.oconnell1@ucdconnect.ie>
2026-04-30 00:28:20 +0100

    Implemented method for user story 1. Returns all newly inferred mappable analogies for the target topic.

--- src/main/java/org/main/InferenceBuilder.java
+++ src/main/java/org/main/InferenceBuilder.java
@@ -1,14 +1,22 @@
 package org.main;
 
-import org.main.Interfaces.Predicate;
 import org.main.Objects.CoalescentMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InferenceBuilder {
 
     public InferenceBuilder(){}
-    public static ArrayList<Predicate> getInferredAnalogies(CoalescentMapping mapping){
-        return new ArrayList<>();
+    public static ArrayList<String> getInferredAnalogies(CoalescentMapping mapping){
+        ArrayList<String> targetTopicAnalogies = AnalogyDataHolder.getAnalogiesFor(mapping.getTarget());
+
+        targetTopicAnalogies = new ArrayList<>(targetTopicAnalogies.stream().filter(x -> !mapping.getCoalescedMapping().containsValue(x)).toList());
+
+        return new ArrayList<>(targetTopicAnalogies.stream().filter(x -> canBeInferred(mapping.getInferredMapping(),x)).toList());
+    }
+
+    private static boolean canBeInferred(HashMap<String,String> inferredMapping, String analogy){
+        return inferredMapping.values().containsAll(AnalogyManager.getUniqueSubjects(analogy));
     }
 }
